04c2d16ecf9c0272f0298605760a30c6a0c5c90e
HippolyteLC <152972861+HippolyteLC@users.noreply.github.com>
2023-12-06 22:44:04 +0100

    added keywords
    added keywords to the sport and study keywords lists;
    added traits/ characteristics to the sports dict

--- assets/keywords.py
+++ assets/keywords.py
@@ -1,19 +1,36 @@
 from typing import List, Dict
 
-keywords_studying: List[str] = ["assignment", "task"]
-keywords_sport: List[str] = ["sport", "football", "basketball", "sport opportunities", "team"]
+# keywords_studying: List[str] = ["assignment", "task"]
+# keywords_sport: List[str] = ["sport", "football", "basketball", "sport opportunities", "team", "game"]
+
+keywords_studying: List[str] = ["textbooks", "notebooks", "paper", "pens", "pencils", "highlighters", 
+    "desk", "chair", "organizational", "laptop", "internet", "charging", 
+    "planner", "timer", "mind", "maps", "summarization", "note", 
+    "online", "databases", "library", "reference", "break", "practice", "mock", 
+    "language", "study", "educational", "grammar", "thesaurus", "writing", "motivational", "visual", 
+    "comfortable", "extra", "external", "whiteboard", "markers", "index", "first", "emergency",
+    "studying", "student", "studies", "students", "learn", "material", "assigment", "task", "study group"
+    "counseling", "essay", "examination", "stressed", "anxious", "credits", "course", "courses", "period",
+    "semester", "report"]
+
+keywords_sport: List[str] = ["sport", "football", "basketball", "sport opportunities", "team", "athlete", "physical", "training",
+    "soccer", "tennis", "korfball", "waterpolo", "swimming", "running", "lifting", "weight", "gym", "rugby", "sailing", "hockey",
+    "rowing", "racing", "formula 1", "baseball", "softball", "handball", "boxing", "golf", "american football", "cricket",
+    "skiing", "teammate", "fighting", "self-defense", "karate", "martial art", "yudo", "zumbda", "yoga", "aikido"]
+
 keywords_social_activities: List[str] = ["social", "social activities", "student association", "events",
                                          "upcoming events", "upcoming social events", "social events"]
+
 sport_keywords: Dict[str, List[str]] = {
-    "aikido": [],
-    "basketball": [],
-    "tennis": [],
-    "swimming": [],
-    "football": [],
-    "zumba": [],
-    "karate": [],
-    "yoga": [],
-    "waterpolo": []
+    "aikido": ["martial arts", "combat", "self-defense"],
+    "basketball": ["team", "ball", "score", "court"],
+    "tennis": ["ball", "racket", "court", "sets"],
+    "swimming": ["water", "physical", "exercise"],
+    "football": ["team", "ball", "score", "field", "goal"],
+    "zumba": ["dance", "physical", "exercise", "rhythm"],
+    "karate": ["martial arts", "combat", "self-defense"],
+    "yoga": ["spiritual", "relax", "stretch", "pose", "breathe"],
+    "waterpolo": ["water", "physical", "exercise", "ball", "team"]
 }
 association_keywords: Dict[str, List[str]] = {
     "Poetry Pals": ["lorem ipsum"],
