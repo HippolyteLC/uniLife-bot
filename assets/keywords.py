2d4370767a3815905af871a70ab6fdfe7fb2128d
HippolyteLC <152972861+HippolyteLC@users.noreply.github.com>
2023-12-06 19:05:07 +0100

    Update keywords.py
    Added more keywords to:
    - study_keywords
    - sport_keywords
    
    Further updates incoming!

--- assets/keywords.py
+++ assets/keywords.py
@@ -1,9 +1,26 @@
 from typing import List, Dict
 
 
-keywords_studying: List[str] = ["assignment", "task"]
-keywords_sport: List[str] = ["sport", "football", "basketball", "sport opportunities", "team"]
+keywords_studying: List[str] = ["textbooks", "notebooks", "paper", "pens", "pencils", "highlighters", 
+    "desk", "chair", "organizational", "laptop", "internet", "charging", 
+    "planner", "timer", "mind", "maps", "summarization", "note", 
+    "online", "databases", "library", "reference", "break", "practice", "mock", 
+    "language", "study", "educational", "grammar", "thesaurus", "writing", "motivational", "visual", 
+    "comfortable", "extra", "external", "whiteboard", "markers", "index", "first", "emergency",
+    "studying", "student", "studies", "students", "learn", "material", "assigment", "task", "study group"
+    "counseling", "essay", "examination", "stressed", "anxious", "credits", "course", "courses", "period",
+    "semester", "report"]
+
+keywords_sport: List[str] = ["sport", "football", "basketball", "sport opportunities", "team", "athlete", "physical", "training",
+    "soccer", "tennis", "korfball", "waterpolo", "swimming", "running", "lifting", "weight", "gym", "rugby", "sailing", "hockey",
+    "rowing", "racing", "formula 1", "baseball", "softball", "handball", "boxing", "golf", "american football", "cricket",
+    "skiing", "teammate", "fighting", "self-defense", "karate", "martial art", "yudo", "zumbda", "yoga", "aikido"]
+available_sport_keywords: List[str] = []
+
+
 keywords_social_activities: List[str] = ["social", "social activities", "student association", "events", "upcoming events", "upcoming social events", "social events"]
+
+
 sport_keywords: Dict[str, List[str]] = {
     "aikido": [],
     "basketball": [],
